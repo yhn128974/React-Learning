c38d6433d9cd3cc547306a70b7956afff77c121d
1289746201 <yuethanhaonan@outlook.com>
2023-03-04 11:41:33 +0800

    路由传参

---
+++ new start/React尚硅谷/第二章：Reat脚手架应用/react_staging/src 路由传参/App.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import MyNavLink from "./components/myNavLink";
+
+import Header from "./components/Header";
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Header></Header>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/* <a className="list-group-item" href="./about.html">
+                About
+              </a>
+              <a className="list-group-item active" href="./home.html">
+                Home
+              </a> */}
+
+              {/* <NavLink
+                activeClassName="active"
+                className="list-group-item active"
+                to="/about"
+              >
+                About
+              </NavLink>
+              <NavLink
+                activeClassName="active"
+                className="list-group-item"
+                to="/home"
+              >
+                Home
+              </NavLink> */}
+
+              <MyNavLink to="/about">About</MyNavLink>
+              <MyNavLink to="/home">Home</MyNavLink>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                {/* 注册路由 */}
+                <Switch>
+                  <Route path="/about" component={About} />
+                  <Route path="/home" component={Home} />
+                  <Redirect to="/about" />
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ new start/React尚硅谷/第二章：Reat脚手架应用/react_staging/src 路由传参/pages/About/index.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+export default class About extends Component {
+  render() {
+    // console.log(`About组件收到的是`,this.props);
+    return <div>About</div>;
+  }
+}
